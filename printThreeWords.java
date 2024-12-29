0875d057ce03ad5d3f1d6504399a75ce2f6cd0e4
Oksana <qasimovaoh@mail.ru>
2024-12-29 16:13:16 +0400

    added changes

---
+++ printThreeWords.java
@@ -0,0 +1,8 @@
+public class printThreeWords {
+    public static void main(String[] args) {
+public static void main(String[args]){
+  System.out.println("Orange" +
+          "Banana" +
+          "Apple");  }
+    }
+}
