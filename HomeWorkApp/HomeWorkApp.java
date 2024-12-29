0d947c47606594aa7b53164f8cfc53c79ca01e14
Oksana <qasimovaoh@mail.ru>
2024-12-29 18:20:58 +0400

    added changes

--- HomeWorkApp/HomeWorkApp.java
+++ HomeWorkApp/HomeWorkApp.java
@@ -2,27 +2,28 @@ package HomeWorkApp;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-    printThreeWords();
-    checkSumSign();
+printThreeWords ();
+checkSumSign ();
     }
 
-    public static void printThreeWords() {
-    System.out.println ("Orange");
-    System.out.println ("Banana");
-    System.out.println ("Apple");
+    public static void printThreeWords (){
+        System.out.println ("Orange");
+        System.out.println ("Banana");
+        System.out.println ("Apple");
     }
 
-    public static void checkSumSign() {
-    int A=5;
-    int B=7;
+    public static void checkSumSign () {
+        int A=5;
+        int B=7;
 
-    if(A+B>=0){
-    System.out.println("Сумма положительная");
-    }
-    if (A+B<0){
-        System.out.println("Сумма отрицательная");
-    }
+        if(A+B>=0){
+            System.out.println("Сумма положительная");
+        }
+        if (A+B<0){
+            System.out.println("Сумма отрицательная");
+        }
+
+
 
     }
-
 }
