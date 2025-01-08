49bcb8880bc6d5f4655e1db64b23c0693816e0b1
Oksana <qasimovaoh@mail.ru>
2025-01-08 16:34:33 +0400

    added changes

--- HomeWorkApp/Lesson_7/Task8.java
+++ HomeWorkApp/Lesson_7/Task8.java
@@ -6,13 +6,14 @@ public class Task8 {
         fillDiagonal();
     }
 
-    public static void fillDiagonal() {
-       int [][] arr = new int [4][4];
+    public static void fillDiagonal(int i, int j) {
+       int [][] arr = new int [i][j];
        for (int i=0; i<4; i++){
            arr [i][i] =1;
            arr [i][4-i-1]=1;{
        for (int j=0; j<4; j++){
             System.out.println(arr[i][j] + " ");
-            System.out.println();
+       }
+               System.out.println();
     }
 }
